267df4c7b9287f0d9cc544b783f69fbb4a3f2d06
Debbie L <debbielefkowitz@gmail.com>
2026-03-14 10:49:23 -0700

    Add TinaCMS generated files

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/Users/debbielefkowitz/Downloads/RotaryWebSiteFiles/Volunteer-Connect-tina/Volunteer-Connect/tina/__generated__/.cache/1773510003245', url: 'https://content.tinajs.io/2.2/content/a85c749f-7fc9-4019-8e3f-8ccb2d0f43a5/github/main', token: 'a720a5bd1b3ec51338cf3583a85cb2a3e0bec409', queries,  });
+export default client;
+  
