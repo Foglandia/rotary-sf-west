17fe3bed3f19dcf877615149e87316e0b3afd3c2
Debbie L <debbielefkowitz@gmail.com>
2026-03-14 12:07:46 -0700

    Remove .env from repository

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/debbielefkowitz/Downloads/RotaryWebSiteFiles/Volunteer-Connect-tina/Volunteer-Connect/tina/__generated__/.cache/1773513476777', url: 'https://content.tinajs.io/2.2/content/8e2ec8a2-bc61-4d2c-bfd0-7e5570731dda/github/main', token: '4ff4355e4f3dc34176f8de0076781c624039789c', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '08c4d3650bc5757a6a2cf05a9951fcc7b040287f', queries,  });
 export default client;
   
